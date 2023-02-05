64cc5f0b36b30d93e0fa7f27972912306f160c8d
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-02-05 15:49:54 +0700

    123

--- resources/views/emails/certificate_neo/mail.blade.php
+++ resources/views/emails/certificate_neo/mail.blade.php
@@ -1,20 +1,12 @@
 @component('mail::message')
       # Dear {{ $participant->name }}, <br>
 
-      <p>
-            Congratulations on your extraordinary outcome of winning the Essay competition. You won the competition and proven to us that you are not continually talking big things that you can fulfill your words as well. You are genuinely a man of work. Keep this diligent work up and attempt to show improvement over this.
-      </p>
-
-      <p>
-            We have also attached the certificate which you can be accessed on this email.
-      <p>
-
+      Congratulations on your extraordinary outcome of winning the Essay competition. You won the competition and proven to us that you are not continually talking big things that you can fulfill your words as well. You are genuinely a man of work. Keep this diligent work up and attempt to show improvement over this.
       
-      <p>  
-            If you have the further question, please contact us at our LINE Official @044wlisy or email neo.bnec@gmail.com
-            <br>
-            Thank you and Have a Great Day
-      </p>
+      We have also attached the certificate which you can be accessed on this email.
+      
+      If you have the further question, please contact us at our LINE Official @044wlisy or email neo.bnec@gmail.com
+      Thank you and Have a Great Day
 
       @component('vendor.mail.text.signature')
       @endcomponent
